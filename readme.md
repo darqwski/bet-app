3893893ce3db5cb3c9965113830a690ee8fe5745
darqwski <dariusz.cabala.pl@gmail.com>
2021-12-18 14:58:08 +0100

    [CT] - add readme.md

---
+++ readme.md
@@ -0,0 +1,5 @@
+#How to run?
+
+1. clone this repo
+2. run `npm install`
+3. run `npm run start`
--- src/components/forms/types.ts
+++ src/components/forms/types.ts
@@ -1,20 +1,3 @@
-import { ChangeEvent } from 'react';
-
-type TSelectData = {value:string, text:string}[]
-
-export interface IFormSelect {
-    data: TSelectData
-    label: string;
-    name: string;
-}
-export interface ISelect {
-    data: TSelectData,
-    name: string
-    dataName?:string,
-    selected?:string;
-    onChange:(event:  ChangeEvent<HTMLSelectElement>) => void
-}
-
 export interface IWithLabel {
     label: string;
     white?: boolean;
@@ -26,10 +9,4 @@ export interface IFormInput {
     white?: boolean;
     type: string;
     disabled?: boolean;
-}
-export interface IFormRadio {
-    label: string;
-    name: string;
-    value: number | string;
-    white?: boolean;
 }
